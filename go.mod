bd92be77fa4a787583bd97d4dd6f6f4204d916ee
Clément Dubreuil <clement@dubreuil.dev>
2026-07-01 21:01:20 +0000

    feat: run all service in a single daemon

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/clementd64/proxy64
 
-go 1.23
+go 1.26
--- main.go
+++ main.go
@@ -1,49 +1,18 @@
 package main
 
 import (
-	"errors"
-	"flag"
+	"context"
 	"fmt"
+	"log/slog"
 	"os"
 
-	"github.com/clementd64/proxy64/internal/http2https"
-	"github.com/clementd64/proxy64/internal/nat64"
+	"github.com/clementd64/proxy64/cmd"
 )
 
-func http2httpsCmd(args []string) error {
-	cmd := flag.NewFlagSet("http2https", flag.ExitOnError)
-	addr := cmd.String("addr", ":80", "address to listen on")
-	cmd.Parse(args)
-
-	return http2https.Listen(*addr)
-}
-
-func nat64Cmd(args []string) error {
-	cmd := flag.NewFlagSet("nat64", flag.ExitOnError)
-	port := cmd.Int("port", 1337, "port to listen on")
-	cmd.Parse(args)
-
-	return nat64.Listen(*port)
-}
-
-func run(args []string) error {
-	if len(args) < 1 {
-		return errors.New("no command provided")
-	}
-
-	switch args[0] {
-	case "http2https":
-		return http2httpsCmd(args[1:])
-	case "nat64":
-		return nat64Cmd(args[1:])
-	default:
-		return errors.New("unknown command")
-	}
-}
-
 func main() {
-	if err := run(os.Args[1:]); err != nil {
-		fmt.Println(err)
+	ctx := context.Background()
+	if err := cmd.Run(ctx, slog.Default(), os.Getenv); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
 }
